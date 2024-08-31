36f59fb6b93a27e8b0353837492222a8c799fb8e
Huda Febrianto Nurrohman <febriantohuda@gmail.com>
2024-09-01 06:19:22 +0700

    new : add save as image in show dicom

--- alzheption/utils.py
+++ alzheption/utils.py
@@ -9,7 +9,7 @@ from tqdm import tqdm
 BASEDIR = os.path.dirname(__file__)
 
 
-def show_dicom(dir_dicom: str) -> dict:
+def show_dicom(dir_dicom: str, save=False) -> dict:
     info = {
         'n_dicom': 0,
         'shape': None,
@@ -21,13 +21,27 @@ def show_dicom(dir_dicom: str) -> dict:
     # --- sort dicom files based on frames sequence.
     paths = sorted(paths, key=lambda x: int(x.split('_')[-3]))
     
+    ii = 0
     for path in tqdm(paths, desc='Showing dicom'):
-        filename = path.split('\\')
+        if save:
+            basepath, classname, _, _, _, _, idname, _ = path.split('\\')
+            
+            basepath = os.path.join(*basepath.split('/')[:-1])
+            classname = classname.split('_')[0]
+            idname = idname.lower()
+            
+            path_idname = os.path.join(basepath, classname, idname)
+            os.makedirs(path_idname, exist_ok=True)
         
         ds = dcmread(path)
         if len(ds.pixel_array.shape) > 2:
             for i in tqdm(range(ds.pixel_array.shape[0]), 'Showing dicom frames'):
                 img = cv.normalize(ds.pixel_array[i], None, 0, 255, cv.NORM_MINMAX).astype(np.uint8)
+                
+                if save:
+                    path_img = os.path.join(path_idname, f"{i}.jpg")
+                    cv.imwrite(path_img, img)
+                
                 cv.imshow('ds', img)
                 if cv.waitKey(30) & 0xFF == ord('q'):
                     break
@@ -38,6 +52,13 @@ def show_dicom(dir_dicom: str) -> dict:
         
         # --- normalize dicom pixel array np.uint16 (0-65535) to np.uint8 (0-255) only for display dicom with opencv.
         img = cv.normalize(ds.pixel_array, None, 0, 255, cv.NORM_MINMAX).astype(np.uint8)
+        
+        if save:
+            path_img = os.path.join(path_idname, f"{ii}.jpg")
+            cv.imwrite(path_img, img)
+        
+        ii += 1
+        
         cv.imshow('ds', img)
         if cv.waitKey(30) & 0xFF == ord('q'):
             break
@@ -66,7 +87,7 @@ if __name__ == '__main__':
         
         print(dirpath)
 
-        info = show_dicom(dirpath)
+        info = show_dicom(dirpath, save=True)
         info.update(
             dict(zip(('Class', 'Source', 'Subject', 'Description', 'AcgDate', 'ID'), dirpath.split('\\')[1:]))
         )
@@ -79,6 +100,8 @@ if __name__ == '__main__':
         
     df = pd.DataFrame(list_info)
     df.to_csv(os.path.join(BASEDIR, 'result', 'show_dicom.csv'), index=False)
+    # df = pd.DataFrame(list_info)
+    # df.to_csv(os.path.join(BASEDIR, 'result', 'show_dicom.csv'), index=False)
     
     # ds = dcmread('D:/Ayang/AD_AXIAL_T2_STAR/ADNI/006_S_4153/Axial_T2-Star/2011-08-03_08_12_01.0/I248520/ADNI_006_S_4153_MR_Axial_T2-Star__br_raw_20110803163318472_1_S117305_I248520.dcm')
     # # ds = dcmread('D:/Ayang/AD_AXIAL_T2_STAR/ADNI/002_S_5018/Axial_T2-Star/2013-11-18_10_41_00.0/I398679/ADNI_002_S_5018_MR_Axial_T2-Star__br_raw_20131118125912373_8_S206235_I398679.dcm')
