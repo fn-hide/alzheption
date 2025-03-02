3c531bd501d9c17380373485f186ba42ca712d83
Huda Febrianto Nurrohman <febriantohuda@gmail.com>
2025-03-02 12:08:40 +0700

    feat: split online and offline train mode

--- alzheption/src/alzheption/utils.py
+++ alzheption/src/alzheption/utils.py
@@ -1,6 +1,9 @@
 import numpy as np
+import tensorflow as tf
 from sklearn.model_selection import RepeatedKFold
 from sklearn.metrics import accuracy_score
+
+from TfELM.Resources.kernel_distances import calculate_pairwise_distances_vector
 
 
 def custom_cross_val_score(model, X, y, n_splits=10, n_repeats=5, scoring=accuracy_score):
@@ -48,3 +51,32 @@ def custom_cross_val_score(model, X, y, n_splits=10, n_repeats=5, scoring=accura
         scores.append(score)
 
     return scores
+
+
+def calc_kernel_batch(x, kernel, batch_size=100):
+    """
+        Calculate the kernel matrix in batches.
+
+        Args:
+        -----------
+            x (tensor): Input data.
+            batch_size (int): Number of samples per batch.
+
+        Returns:
+        -----------
+            tensor: Kernel matrix values.
+    """
+    x = tf.cast(x, dtype=tf.float32)  # Pastikan format float32
+    
+    # Jika tidak ada data sebelumnya, buat kernel identity
+    identity = tf.ones((tf.shape(x)[0], 1), dtype=tf.float32)  
+    
+    num_samples = tf.shape(x)[0]
+    kernel_matrix = []
+    
+    for i in range(0, num_samples, batch_size):
+        x_batch = x[i : i + batch_size]  # Ambil batch data
+        k_batch = calculate_pairwise_distances_vector(x_batch, identity, kernel.ev)  
+        kernel_matrix.append(k_batch)  # Simpan hasil batch
+    
+    return tf.concat(kernel_matrix, axis=0)  # Gabungkan semua batch
